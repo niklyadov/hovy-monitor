8a45581d167b68e36c2ceb2aa2252d43e5080803
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2022-08-24 16:14:44 +0500

    Update default configs settings

--- HovyMonitor.Api/Entity/Configuration.cs
+++ HovyMonitor.Api/Entity/Configuration.cs
@@ -16,10 +16,9 @@ namespace HovyMonitor.Api.Entity
 
     public class SerialPortConfiguration
     {
-        public string Name { get; set; } = "COM4";
-        public int BaundRate { get; set; } = 9600;
+        public int BaundRate { get; set; } = 115200;
         public byte DataBits { set; get; } = 8;
         public int ReadTimeout { get; set; } = 2000;
-        public int SendInterval { get; set; } = 2000;
+        public int SendInterval { get; set; } = 5000;
     }
 }
